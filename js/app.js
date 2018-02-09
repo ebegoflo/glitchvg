d2b86a6f75cea5db18c181b1ab5a6c6b26909458
Samahara <le.chat.au.chapeau@hotmail.com>
2018-02-09 01:14:41 -0600

    actualizando reedirección entre páginas

--- js/app.js
+++ js/app.js
@@ -1,5 +1,19 @@
-$(document).ready(function(){
-      // $('.carousel').carousel();
-      $('.slider').slider({fullWidth: true});
-       $(".button-collapse").sideNav();
-    });
+function loadPage() {
+  $('.slider').slider({fullWidth: true});
+  $('.button-collapse').sideNav();
+  $('.optionHome').click(changePage);
+}
+
+function changePage() {
+  var $option = event.target.id;
+  console.log($option);
+  if($option === "articles"){
+    window.location = "views/article.html";
+  } else if ($option === "reviews") {
+    window.location = "views/reviews.html";
+  } else if ($option === "logo-container") {
+    window.location = "index.html";
+  }
+}
+
+$(document).ready(loadPage);
